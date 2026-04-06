32cdfa77a707c7915dc3e7fea66365f4f51831a8
Cefz Zendejas <molanddise@gmail.com>
2026-04-05 22:02:15 -0600

    Finised couse

--- src/app/home/components/search/search.component.ts
+++ src/app/home/components/search/search.component.ts
@@ -1,11 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+
+import { ReactiveFormsModule } from '@angular/forms';
+import { map, debounceTime } from 'rxjs/operators';
 
 @Component({
   selector: 'app-search',
-  imports: [],
+  imports: [
+    ReactiveFormsModule
+  ],
   templateUrl: './search.component.html',
   styleUrl: './search.component.scss'
 })
 export class SearchComponent {
+
+  searchField = new FormControl;
+  results: any[] = [];
+
+  http = inject(HttpClient);
+
+  ngOnInit(): void{
+    this.searchField.valueChanges
+    .pipe(debounceTime(300))
+    .subscribe( value => {
+      this.getData(value);
+    })
+  }
+
+  private getData(query: string) {
+    const API = 'fRqE7Z6ywo5g4G7H6cM7nhFsyAka2hJw'
+    this.http.get(`https://api.giphy.com/v1/gifs/search?q=${query}&api_key=${API}&limit=12`)
+    .pipe(
+      map((response:  any) => {
+        return response.data.map(item => item.images.downsized)
+      })
+    )
+    .subscribe((data) => {
+      this.results = data;
+    })
+    ;
+  }
 
 }
